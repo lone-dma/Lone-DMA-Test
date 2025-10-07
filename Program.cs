1ea38f379db30d0f56dadf4206856abb8e76f7ca
Lone <234549977+lone7254@users.noreply.github.com>
2025-10-06 20:22:23 -0500

    Version 2.9 Support Win11 25H2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using LoneDMATest.Tests;
 using System.Reflection;
 using System.Text;
 
-[assembly: AssemblyVersion("2.8.*")]
+[assembly: AssemblyVersion("2.9.*")]
 [assembly: AssemblyTitle(Program.Name)]
 [assembly: AssemblyProduct(Program.Name)]
 [assembly: AssemblyCopyright("©2025 Lone")]
