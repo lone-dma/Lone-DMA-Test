7e729d1ee934c23c8e48d2898c4dae83013edde1
Lone <234549977+lone7254@users.noreply.github.com>
2025-11-05 15:31:01 -0600

    Refactor pause logic and add Utilities class
    Centralized the "pause and wait for user input" functionality by introducing a `ConsolePause()` method in a new `Utilities` class within the `LoneDMATest.Misc` namespace. Replaced redundant `AnsiConsole.MarkupLine` and `Console.ReadKey` calls in `Program.cs` with this utility method.
    
    Added several `using` directives in `Program.cs` to include additional namespaces and libraries. Updated exception handling and cleanup code to use the new utility method, improving code maintainability, readability, and consistency.
    [no ci]

---
+++ src/Misc/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+
+namespace LoneDMATest.Misc
+{
+    internal static class Utilities
+    {
+        public static void ConsolePause()
+        {
+            AnsiConsole.MarkupLine("[grey]Press any key to continue...[/]");
+            Console.ReadKey(intercept: true);
+        }
+    }
+}
